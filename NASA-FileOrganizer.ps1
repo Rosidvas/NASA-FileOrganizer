ad3e45ca82c889c1a24f91ca38564f56812b2b80
Rosidvas <89085444+Rosidvas@users.noreply.github.com>
2022-06-20 15:04:46 +0200

    Update NASA-FileOrganizer.ps1

--- NASA-FileOrganizer.ps1
+++ NASA-FileOrganizer.ps1
@@ -1,44 +1,27 @@
-{
-<# ----NASA-Class file organizer---- #>
-<# Author: Studer                    
-<# Version: 0.0.1                    
-<# Date: 13/06/2022                  
-<# Description                       
-<# Very swift and efficient file organizer     
-<# graded by the finest rocket scientists at NASA
-<#----------------------------------------------------#>
 
+    <# ----NASA-Class file organizer---- #>
+    <# Author: Studer                    
+    <  # Version: 0.0.1                    
+    <# Date: 13/06/2022                  
+    <# Description                       
+    <# Very swift and efficient file organizer     
+    <# graded by the finest rocket scientists at NASA
+    <#----------------------------------------------------#>
 
+    $path = "C:\users\rando\downloads"
+    $picturePath = "C:\Users\rando\OneDrive\Pictures\Saved Pictures"
+    $countjpeg = 0
 
-
-
-
-$path = "C:\users\rando\downloads"
-$folderPath = "C:\Users\rando\OneDrive\Documents\Schule\BBBaden"
-$picturePath = "C:\Users\rando\OneDrive\Pictures\Saved Pictures"
-$unorganized = 0
-$countjpeg = 0
-
-$fileNum = @(Get-ChildItem $path)
-foreach ($files in $fileNum)
-{
-    $folderName = @(Get-ChildItem $folderPath -Recurse -Directory)
-    }
-    $extn = [System.IO.Path]::GetExtension($files)
-   
-    if ($extn -eq '.jpeg')
+    $fileNum = @(Get-ChildItem $path)
+    foreach ($files in $fileNum)
     {
-        Move-Item $files -Destination $picturePath
+    
+        $extn = [System.IO.Path]::GetExtension($files)
+   
+        if ($extn -eq '.jpg')
+        {
+        Move-Item -path $path $files -Destination $picturePath
         $countjpeg++
         Write-Host "Picture moved successfully" -ForegroundColor Yellow
+        }
     }
-
-    if ($files -match $folderName)
-    {
-        ++$unorganized
-    }
-    
-    $unorganized
-    $countjpeg
-    
-}
